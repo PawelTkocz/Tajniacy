84f777abd97c9e86cc61263f5a5cf705e4f52ec6
Leon <lelek0801@gmail.com>
2024-02-05 17:13:25 +0100

    Dodanie pliku ze słowami oraz funckji losujących słowa dla drużyn

--- Server/game.js
+++ Server/game.js
@@ -6,28 +6,73 @@ module.exports = {
     makeRoomId
 }
 
+function generateUniqueWords(filePath, gridSize) {
+    const fs = require('fs');
+    const words = fs.readFileSync(filePath, 'utf-8').split('\n');
+    const grid = [];
+
+    if (words.length < gridSize * gridSize) {
+        throw new Error('Insufficient words in the file');
+    }
+
+    const usedIndices = new Set();
+
+    for (let i = 0; i < gridSize; i++) {
+        const row = [];
+        for (let j = 0; j < gridSize; j++) {
+            let randomIndex;
+            do {
+                randomIndex = Math.floor(Math.random() * words.length);
+            } while (usedIndices.has(randomIndex));
+            usedIndices.add(randomIndex);
+            row.push(words[randomIndex]);
+        }
+        grid.push(row);
+    }
+
+    return grid;
+}
+
+function generateAgentsIdentities(GRID_SIZE) {
+    const agentsIdentities = [];
+    const totalCells = GRID_SIZE * GRID_SIZE;
+    const onesCount = 8;
+    const twosCount = 7;
+    const threesCount = 1;
+    const zerosCount = totalCells - onesCount - twosCount - threesCount;
+
+    const identities = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3];
+    for (let i = 0; i < zerosCount; i++) {
+        identities.push(0);
+    }
+
+    for (let i = identities.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [identities[i], identities[j]] = [identities[j], identities[i]];
+    }
+
+    for (let i = 0; i < GRID_SIZE; i++) {
+        const row = [];
+        for (let j = 0; j < GRID_SIZE; j++) {
+            row.push(identities[i * GRID_SIZE + j]);
+        }
+        agentsIdentities.push(row);
+    }
+
+    return agentsIdentities;
+}
+
 function initGame() {
-    let generatedWords = [
-        ['kot', 'liść', 'klawiatura', 'palec', 'wieża'],
-        ['kolano', 'pies', 'korona', 'krasnal', 'plastik'],
-        ['egzamin', 'poduszka', 'dom', 'łyżwa', 'mięsień'],
-        ['zeszyt', 'ściąga', 'prezent', 'zlew', 'śnieg'],
-        ['Egipt', 'karton', 'bramka', 'hasło', 'igła']
-    ];
-    let agentsIdentities = [
-        [1, 1, 1, 2, 0],
-        [0, 2, 1, 0, 0],
-        [1, 2, 3, 0, 0],
-        [0, 0, 0, 2, 1],
-        [2, 1, 1, 2, 2]
-    ];
-    let revealedIdentities = [
-        [0, 0, 1, 1, 1],
-        [1, 1, 1, 0, 1],
-        [0, 0, 0, 1, 0],
-        [0, 0, 0, 1, 1],
-        [1, 0, 1, 1, 0]
-    ];
+    let generatedWords = generateUniqueWords('../Static/words.txt', GRID_SIZE);
+    let agentsIdentities = generateAgentsIdentities(GRID_SIZE);
+
+    // let revealedIdentities = [
+    //     [0, 0, 1, 1, 1],
+    //     [1, 1, 1, 0, 1],
+    //     [0, 0, 0, 1, 0],
+    //     [0, 0, 0, 1, 1],
+    //     [1, 0, 1, 1, 0]
+    // ];
 
     return {
         teams: [{
@@ -50,7 +95,7 @@ function initGame() {
         playersToRoles: {},
         words: generatedWords,
         agentsIdentities: agentsIdentities,
-        revealedIdentities: revealedIdentities,
+        // revealedIdentities: revealedIdentities,
         gridsize: GRID_SIZE,
         currentTurn: 1
     };
